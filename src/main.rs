de45e222e85aaba443517a8393ecd62c4e7a8662
Saisana299 <yt0521kariaka@gmail.com>
2023-09-13 13:44:40 +0900

    Add Files

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::{thread, io, process::exit};
+use tello_autopilot::tello::Tello;
+
+fn main() {
+    let local_ip = "192.168.10.2";
+    let tello_ip = "192.168.10.1";
+    let tello = Tello::new(15, local_ip, tello_ip);
+
+    // 接続チェック
+    let result = tello.send_cmd("command", true);
+    if !result {
+        println!("Telloとの接続を確認してください。");
+        exit(0);
+    }
+    tello.listen_state();
+
+    // 入力ループ
+    let handle = thread::spawn(move || {
+        loop {
+            let mut str = String::new();
+            io::stdin().read_line(&mut str).expect("failed to read line");
+            tello.send_cmd(&str.lines().collect::<String>(), false);
+        }
+    });
+
+    handle.join().unwrap();
+}
