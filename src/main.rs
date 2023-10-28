42641a749204af51ca6e987ba6d5f3f5b0d505f2
Zakki0925224 <miyazakiyuuta15@gmail.com>
2023-10-28 17:59:26 +0900

    [Fix]: Fixed cmd listener

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,92 @@
 use log::{error, info};
-use std::{env, sync::Arc, time::Duration};
+use std::{env, io::ErrorKind, time::Duration};
 use tello_autopilot::{cmd::Command, state::State};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream, UdpSocket},
-    sync::Mutex,
 };
 
-const LOCAL_IP: &'static str = "0.0.0.0";
-const TELLO_IP: &'static str = "192.168.10.1";
+// const LOCAL_IP: &'static str = "0.0.0.0";
+// const TELLO_IP: &'static str = "192.168.10.1";
 
-const TELLO_CMD_PORT: u16 = 8889;
-const TELLO_STATE_PORT: u16 = 8890;
-const TELLO_VIDEO_STREAM_PORT: u16 = 11111;
-const TELLO_STREAM_ACCESS_PORT: u16 = 62512;
+// const TELLO_CMD_PORT: u16 = 8889;
+// const TELLO_STATE_PORT: u16 = 8890;
+// const TELLO_VIDEO_STREAM_PORT: u16 = 11111;
+// const TELLO_STREAM_ACCESS_PORT: u16 = 62512;
 
-const SERVER_CMD_PORT: u16 = 8989;
-const SERVER_STATE_PORT: u16 = 8990;
+// const SERVER_CMD_PORT: u16 = 8989;
+// const SERVER_STATE_PORT: u16 = 8990;
+const LISTEN_CMD_ADDR: (&'static str, u16) = ("127.0.0.1", 8989);
+const LISTEN_STATE_ADDR: (&'static str, u16) = ("127.0.0.1", 8990);
 
-const TIMEOUT_MS: u64 = 100;
+const TIMEOUT_MS: u64 = 15000; // 15s
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    async fn sleep() {
-        tokio::time::sleep(tokio::time::Duration::from_millis(TIMEOUT_MS as u64)).await;
+    async fn sleep_3s() {
+        tokio::time::sleep(tokio::time::Duration::from_millis(3000)).await;
     }
 
-    async fn send_cmd(cmd: Command) -> Result<(), Box<dyn std::error::Error>> {
-        shoot_cmd(("127.0.0.1", SERVER_CMD_PORT), cmd).await
+    // TODO: not working
+    fn send_cmd(cmd: Command) {
+        tokio::spawn(async move {
+            if let Err(e) = shoot_cmd(LISTEN_CMD_ADDR, &cmd).await {
+                error!("sned cmd: {:?}", e);
+            }
+        });
     }
 
     env::set_var("RUST_LOG", "info");
     env_logger::init();
 
+    // command
     tokio::spawn(async move {
-        if let Err(e) =
-            listen_and_send_cmd((LOCAL_IP, SERVER_CMD_PORT), (TELLO_IP, TELLO_CMD_PORT)).await
-        {
+        if let Err(e) = listen_and_send_cmd(LISTEN_CMD_ADDR, ("192.168.10.1", 8889)).await {
             error!("listen cmd: {:?}", e);
         }
     });
-
-    //sleep().await;
-    tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
-    send_cmd(Command::Command).await?;
-    send_cmd(Command::StreamOn).await?;
+    sleep_3s().await;
 
     tokio::spawn(async move {
-        if let Err(e) = listen_stdin(("127.0.0.1", SERVER_CMD_PORT)).await {
+        if let Err(e) = listen_stdin(LISTEN_CMD_ADDR).await {
             error!("listen stdin: {:?}", e);
         }
     });
 
-    // tokio::spawn(async move {
-    //     if let Err(e) =
-    //         shoot_cmd_infinitely(("127.0.0.1", SERVER_CMD_PORT), Command::Command, 10000).await
-    //     {
-    //         error!("listen shoot cmd: {:?}", e);
-    //     }
-    // });
-
-    sleep().await;
+    //send_cmd(Command::Command);
 
     tokio::spawn(async move {
-        if let Err(e) =
-            listen_and_send_state((LOCAL_IP, SERVER_STATE_PORT), (TELLO_IP, TELLO_STATE_PORT)).await
-        {
+        if let Err(e) = shoot_cmd_infinitely(LISTEN_CMD_ADDR, &Command::Command, 15000).await {
+            error!("listen shoot cmd: {:?}", e);
+        }
+    });
+
+    sleep_3s().await;
+
+    tokio::spawn(async move {
+        if let Err(e) = listen_and_send_state(LISTEN_STATE_ADDR, ("0.0.0.0", 8890)).await {
             error!("Error in listen state thread: {:?}", e);
         }
     });
 
-    tokio::spawn(async move {
-        if let Err(e) = listen_and_stream_video(
-            (LOCAL_IP, TELLO_VIDEO_STREAM_PORT),
-            (TELLO_IP, TELLO_STREAM_ACCESS_PORT),
-            &[
-                ("127.0.0.1", 11112), // watchdog
-                ("127.0.0.1", 11113), // detector
-            ],
-        )
-        .await
-        {
-            error!("listen video: {:?}", e);
-        }
-    });
+    // video
+    //send_cmd(Command::StreamOn);
 
-    // メインスレッドが終了しないように待機
+    // tokio::spawn(async move {
+    //     if let Err(e) = listen_and_stream_video(
+    //         ("0.0.0.0", 11111),
+    //         ("192.168.10.1", 62512),
+    //         &[
+    //             ("127.0.0.1", 11112), // watchdog
+    //             ("127.0.0.1", 11113), // detector
+    //         ],
+    //     )
+    //     .await
+    //     {
+    //         error!("listen video: {:?}", e);
+    //     }
+    // });
+
     tokio::signal::ctrl_c().await?;
 
     Ok(())
@@ -110,7 +111,6 @@ async fn listen_and_send_cmd<A: tokio::net::ToSocketAddrs + Copy + Send + 'stati
         info!("listen cmd: Connected from {}", addr);
 
         tokio::spawn(async move {
-            // receive cmd and send to dest target (is drone)
             loop {
                 let size = match stream.read(&mut buf).await {
                     Ok(size) => size,
@@ -124,7 +124,7 @@ async fn listen_and_send_cmd<A: tokio::net::ToSocketAddrs + Copy + Send + 'stati
                 };
 
                 if size == 0 {
-                    break;
+                    continue;
                 }
 
                 let data = &buf[..size];
@@ -135,6 +135,7 @@ async fn listen_and_send_cmd<A: tokio::net::ToSocketAddrs + Copy + Send + 'stati
                     None => {
                         error!("Invalid command: \"{}\"", s);
                         stream.write_all("error".as_bytes()).await.unwrap();
+                        buf.fill(0);
                         continue;
                     }
                 };
@@ -150,6 +151,7 @@ async fn listen_and_send_cmd<A: tokio::net::ToSocketAddrs + Copy + Send + 'stati
                 {
                     error!("listen cmd: Failed to send cmd to target: {:?}", e);
                     stream.write_all("error".as_bytes()).await.unwrap();
+                    buf.fill(0);
                     continue;
                 }
 
@@ -162,20 +164,23 @@ async fn listen_and_send_cmd<A: tokio::net::ToSocketAddrs + Copy + Send + 'stati
                             e
                         );
                         stream.write_all("error".as_bytes()).await.unwrap();
-                        return;
+                        buf.fill(0);
+                        continue;
                     }
                 };
 
                 let s = String::from_utf8_lossy(&buf[..size]);
                 info!("listen cmd: Receive response from target: {:?}", s);
                 stream.write_all(s.as_bytes()).await.unwrap();
+
+                buf.fill(0);
             }
             info!("listen cmd: End of connection with client ({})", addr);
         });
     }
 }
 
-async fn listen_stdin<A: tokio::net::ToSocketAddrs>(
+async fn listen_stdin<A: tokio::net::ToSocketAddrs + Copy>(
     target: A,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let stdin = async_std::io::stdin();
@@ -188,14 +193,27 @@ async fn listen_stdin<A: tokio::net::ToSocketAddrs>(
             continue;
         }
 
-        stream.write_all(line.trim().as_bytes()).await?;
+        let s = line.trim();
+        let cmd = match Command::from_str(s) {
+            Some(cmd) => cmd,
+            None => {
+                error!("Invalid command: \"{}\"", s);
+                line.clear();
+                continue;
+            }
+        };
+
+        if let Err(e) = stream.write_all(cmd.to_string().as_bytes()).await {
+            error!("{:?}", e);
+        }
+
         line.clear();
     }
 }
 
 async fn shoot_cmd<A: tokio::net::ToSocketAddrs>(
     target: A,
-    cmd: Command,
+    cmd: &Command,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let mut stream = TcpStream::connect(target).await?;
     stream.write_all(cmd.to_string().as_bytes()).await?;
@@ -203,16 +221,16 @@ async fn shoot_cmd<A: tokio::net::ToSocketAddrs>(
     Ok(())
 }
 
-async fn shoot_cmd_infinitely<A: tokio::net::ToSocketAddrs>(
+async fn shoot_cmd_infinitely<A: tokio::net::ToSocketAddrs + Copy>(
     target: A,
-    cmd: Command,
+    cmd: &Command,
     dur_ms: usize,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let mut stream = TcpStream::connect(target).await?;
-    let cmd_str = cmd.to_string();
+    let s = cmd.to_string();
 
     loop {
-        stream.write_all(cmd_str.as_bytes()).await?;
+        stream.write_all(s.as_bytes()).await?;
         tokio::time::sleep(tokio::time::Duration::from_millis(dur_ms as u64)).await;
     }
 }
@@ -221,79 +239,26 @@ async fn listen_and_send_state<A: tokio::net::ToSocketAddrs + Copy + Send + 'sta
     listen_target: A,
     src_target: A,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    // let listener = TcpListener::bind(listen_target).await?;
-
-    // // multi clients
-    // loop {
-    //     info!("listen state: Waiting connection...");
-    //     let (mut stream, addr) = match listener.accept().await {
-    //         Ok(r) => r,
-    //         Err(e) => return Err(Box::new(e)),
-    //     };
-
-    //     let src_socket = UdpSocket::bind(src_target).await?;
-
-    //     info!("listen state: Connected from {}", addr);
-
-    //     tokio::spawn(async move {
-    //         let mut buf = vec![0; 1024];
-
-    //         loop {
-    //             // wait response
-    //             if tokio::time::timeout(Duration::from_millis(TIMEOUT_MS), async {
-    //                 let size = match src_socket.recv_from(&mut buf).await {
-    //                     Ok((size, _)) => size,
-    //                     Err(e) => {
-    //                         error!(
-    //                             "listen state: Failed to receive response from target: {:?}",
-    //                             e
-    //                         );
-    //                         return;
-    //                     }
-    //                 };
-
-    //                 let s = String::from_utf8_lossy(&buf[..size]).to_string();
-    //                 let state = State::from_str(s.as_str());
-    //                 info!("listen state: Receive state from target: {:?}", state);
-    //                 stream
-    //                     .write_all(serde_json::to_string(&state).unwrap().as_bytes())
-    //                     .await
-    //                     .unwrap();
-    //             })
-    //             .await
-    //             .is_err()
-    //             {
-    //                 error!("listen state: Response timed out from target");
-    //             }
-    //         }
-    //         //info!("listen state: End of connection with client ({})", addr);
-    //     });
-    // }
+    //let listener = TcpListener::bind(listen_target).await?;
     let src_socket = UdpSocket::bind(src_target).await?;
     let mut buf = vec![0; 1024];
 
     loop {
-        if tokio::time::timeout(Duration::from_millis(TIMEOUT_MS), async {
-            let size = match src_socket.recv_from(&mut buf).await {
-                Ok((size, _)) => size,
-                Err(e) => {
-                    error!(
-                        "listen state: Failed to receive response from target: {:?}",
-                        e
-                    );
-                    return;
-                }
-            };
+        let size = match src_socket.recv_from(&mut buf).await {
+            Ok((size, _)) => size,
+            Err(e) => {
+                error!(
+                    "listen state: Failed to receive response from target: {:?}",
+                    e
+                );
+                continue;
+            }
+        };
 
-            let s = String::from_utf8_lossy(&buf[..size]).to_string();
-            let state = State::from_str(s.as_str());
-            info!("listen state: Receive state from target: {:?}", state);
-        })
-        .await
-        .is_err()
-        {
-            error!("listen state: Response timed out from target");
-        }
+        let s = String::from_utf8_lossy(&buf[..size]).to_string();
+        let state = State::from_str(s.as_str());
+        info!("listen state: Receive state from target: {:?}", state);
+        buf.fill(0);
     }
 }
 
